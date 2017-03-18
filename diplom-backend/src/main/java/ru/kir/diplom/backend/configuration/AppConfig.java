d660b7c1d6d7388b281b25fb3320f747dbe4c4d6
KirillPamPam <kirill.mai308@gmaio.com>
2017-03-18 20:24:17 +0300

    Added spring mvc config

--- diplom-backend/src/main/java/ru/kir/diplom/backend/configuration/AppConfig.java
+++ diplom-backend/src/main/java/ru/kir/diplom/backend/configuration/AppConfig.java
@@ -3,12 +3,14 @@ package ru.kir.diplom.backend.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Created by Kirill Zhitelev on 15.03.2017.
  */
 @Configuration
 @ComponentScan(basePackages = "ru.kir.diplom.backend.*")
+@EnableWebMvc
 @Import({HibernateConfiguration.class})
 public class AppConfig {
 }
